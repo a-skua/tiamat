1b3e612b8d920bcd6fb15e3f15073150fbb5b69b
asuka yagi <yagi.asuka.0x06@gmail.com>
2020-12-15 00:43:52 +0900

    add instruction; add arithmetic memory

--- lib/src/instruction.dart
+++ lib/src/instruction.dart
@@ -6,6 +6,7 @@ class Instruction {
   final store = _store;
   final loadAddress = _loadAddress;
   final load = _load;
+  final addArithmeticMemory = _addArithmeticMemory;
 }
 
 /// NOP
@@ -29,7 +30,7 @@ void _loadMemory(final Resource r) {
   if (data == 0) {
     flag |= zeroFlag;
   }
-  if ((data & (1 << 15)) > 0) {
+  if ((data & (1 << (wordSize - 1))) > 0) {
     flag |= signFlag;
   }
   r.setGR(reg, data);
@@ -85,19 +86,42 @@ void _loadAddress(final Resource r) {
 }
 
 /// ADDA r, adr, x
-// void _addArithmeticMemory(final Resource r) {
-//   final cache = r.memory.getWord(r.PR);
-//   r.PR += 1;
-//
-//   final x = cache & 0xf;
-//   final reg = (cache >> 4) & 0xf;
-//   final adr =
-//       x == 0 ? r.memory.getWord(r.PR) : r.memory.getWord(r.PR) + r.getGR(x);
-//   r.PR += 1;
-//
-//   final result = r.getGR(reg) + r.memory.getWord(adr);
-//   r.setGR(reg, result);
-// }
+void _addArithmeticMemory(final Resource r) {
+  final cache = r.memory.getWord(r.PR);
+  r.PR += 1;
+
+  final x = cache & 0xf;
+  final gr = (cache >> 4) & 0xf;
+  final adr =
+      x == 0 ? r.memory.getWord(r.PR) : r.memory.getWord(r.PR) + r.getGR(x);
+  r.PR += 1;
+
+  const signMask = 1 << (wordSize - 1);
+  const maskBits = (1 << wordSize) - 1;
+  const overflowMask = -1 ^ maskBits;
+
+  final v1 = r.getGR(gr);
+  final v2 = r.memory.getWord(adr);
+  final raw = v1 + v2;
+  final result = raw & maskBits;
+
+  var flag = 0;
+  if ((raw & overflowFlag) > 0) {
+    flag |= overflowFlag;
+  } else if (((v1 & signMask) ^ (v2 & signMask)) == 0) {
+    if ((v1 & signMask) != (result & signMask)) {
+      flag |= overflowFlag;
+    }
+  }
+  if ((result & signMask) > 0) {
+    flag |= signFlag;
+  }
+  if (result == 0) {
+    flag |= zeroFlag;
+  }
+  r.setGR(gr, result);
+  r.FR = flag;
+}
 
 /// ADDA r1, r2
 // void _addArithmetic(final Resource r) {
---
+++ test/instruction_addx_test.dart
@@ -0,0 +1,166 @@
+import 'dart:math';
+
+import 'package:tiamat/src/instruction.dart';
+import 'package:tiamat/src/resource.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final rand = Random();
+
+  group('add arithmetic memory', () {
+    test('without base', () {
+      final r = Resource();
+      final ins = Instruction();
+
+      for (var i = 0; i < 3; i++) {
+        final reg = rand.nextInt(8);
+        final pr = rand.nextInt(1 << 15);
+        final op = (0x20 << 8) + (reg << 4);
+        final addr = rand.nextInt(1 << 15) + (1 << 15);
+        final v1 = rand.nextInt(1 << 15);
+        final v2 = rand.nextInt(1 << 15);
+
+        r.PR = pr;
+        r.setGR(reg, v1);
+        r.memory.setWord(pr, op);
+        r.memory.setWord(pr + 1, addr);
+        r.memory.setWord(addr, v2);
+        ins.addArithmeticMemory(r);
+        expect(r.getGR(reg), equals(v1 + v2));
+      }
+    });
+
+    test('with base', () {
+      final r = Resource();
+      final ins = Instruction();
+
+      for (var i = 0; i < 3; i++) {
+        final reg = rand.nextInt(8);
+        var baseReg = rand.nextInt(7) + 1;
+        if (reg == baseReg) {
+          baseReg = baseReg == 7 ? 1 : baseReg + 1;
+        }
+        final pr = rand.nextInt(1 << 15) + (1 << 15);
+        final op = (0x20 << 8) + (reg << 4);
+        final addr = rand.nextInt(1 << 14);
+        final base = rand.nextInt(1 << 14);
+        final v1 = rand.nextInt(1 << 15);
+        final v2 = rand.nextInt(1 << 15);
+
+        r.PR = pr;
+        r.setGR(reg, v1);
+        r.setGR(baseReg, base);
+        r.memory.setWord(pr, op);
+        r.memory.setWord(pr + 1, addr);
+        r.memory.setWord(addr, v2);
+        ins.addArithmeticMemory(r);
+        expect(r.getGR(reg), equals(v1 + v2));
+      }
+    });
+
+    group('flags', () {
+      test('overflow', () {
+        const maskBits = 0xffff;
+        final r = Resource();
+        final ins = Instruction();
+
+        // (-) + (-)
+        for (var i = 0; i < 4; i++) {
+          final addr = rand.nextInt(1 << 15) + (1 << 15);
+          final v1 = 1 << 15;
+          final v2 = rand.nextInt(1 << 15) + (1 << 15);
+          final gr = rand.nextInt(8);
+          final op = (0x20 << 8) + (gr << 4);
+          final result = (v1 + v2) & maskBits;
+
+          r.setGR(gr, v1);
+          r.memory.setWord(r.PR, op);
+          r.memory.setWord(r.PR + 1, addr);
+          r.memory.setWord(addr, v2);
+
+          ins.addArithmeticMemory(r);
+          expect(r.OF, equals(true));
+          expect(r.SF, equals((result & (1 << 15)) > 0));
+          expect(r.ZF, equals(result == 0));
+        }
+
+        // (+) + (+)
+        for (var i = 0; i < 4; i++) {
+          final addr = rand.nextInt(1 << 15) + (1 << 15);
+          final v1 = 1 << 14;
+          final v2 = rand.nextInt(1 << 14) + (1 << 14);
+          final gr = rand.nextInt(8);
+          final op = (0x20 << 8) + (gr << 4);
+          final result = (v1 + v2) & maskBits;
+
+          r.setGR(gr, v1);
+          r.memory.setWord(r.PR, op);
+          r.memory.setWord(r.PR + 1, addr);
+          r.memory.setWord(addr, v2);
+
+          ins.addArithmeticMemory(r);
+          expect(r.getGR(gr), equals(result));
+          expect(r.OF, equals(true));
+          expect(r.SF, equals((result & (1 << 15)) > 0));
+          expect(r.ZF, equals(result == 0));
+        }
+      });
+
+      test('zefo', () {
+        final r = Resource();
+        final ins = Instruction();
+
+        var addr = rand.nextInt(1 << 15) + (1 << 15);
+        var v1 = 1 << 15;
+        var v2 = 1 << 15;
+        var gr = rand.nextInt(8);
+        var op = (0x20 << 8) + (gr << 4);
+
+        r.setGR(gr, v1);
+        r.memory.setWord(r.PR, op);
+        r.memory.setWord(r.PR + 1, addr);
+        r.memory.setWord(addr, v2);
+
+        ins.addArithmeticMemory(r);
+        expect(r.getGR(gr), equals(0));
+        expect(r.OF, equals(true));
+        expect(r.SF, equals(false));
+        expect(r.ZF, equals(true));
+
+        addr = rand.nextInt(1 << 15) + (1 << 15);
+        v1 = 0;
+        v2 = 0;
+        gr = rand.nextInt(8);
+        op = (0x20 << 8) + (gr << 4);
+
+        r.setGR(gr, v1);
+        r.memory.setWord(r.PR, op);
+        r.memory.setWord(r.PR + 1, addr);
+        r.memory.setWord(addr, v2);
+
+        ins.addArithmeticMemory(r);
+        expect(r.getGR(gr), equals(0));
+        expect(r.OF, equals(false));
+        expect(r.SF, equals(false));
+        expect(r.ZF, equals(true));
+
+        addr = rand.nextInt(1 << 15) + (1 << 15);
+        v1 = 1 << 14;
+        v2 = ((1 << 14) ^ -1) + 1;
+        gr = rand.nextInt(8);
+        op = (0x20 << 8) + (gr << 4);
+
+        r.setGR(gr, v1);
+        r.memory.setWord(r.PR, op);
+        r.memory.setWord(r.PR + 1, addr);
+        r.memory.setWord(addr, v2);
+
+        ins.addArithmeticMemory(r);
+        expect(r.getGR(gr), equals(0));
+        expect(r.OF, equals(false));
+        expect(r.SF, equals(false));
+        expect(r.ZF, equals(true));
+      });
+    });
+  });
+}
