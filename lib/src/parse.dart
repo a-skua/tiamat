52f60ae44f8a0cdbd5a642885a40c91a3c922e03
asuka yagi <yagi.asuka.0x06@gmail.com>
2020-12-22 00:12:01 +0900

    add new method

--- lib/src/parse.dart
+++ lib/src/parse.dart
@@ -1,3 +1,21 @@
+typedef ReplaceLabel = List<int> Function(String label, int addr);
+
+class Token {
+  final List<int> code;
+  final String label;
+  final String refLabel;
+
+  Token(
+    this.code, {
+    this.label = '',
+    this.refLabel = '',
+  });
+
+  int get size => this.code.length;
+  bool get hasLabel => this.label.isNotEmpty;
+  bool get hasReferenceLabel => this.refLabel.isNotEmpty;
+}
+
 class Parse {
   // FIXME
   //
@@ -9,36 +27,53 @@ class Parse {
   final _expADR = RegExp(r'^(#?[0-9]+)$');
 
   List<int> exec(final String s) {
-    final List<int> instructions = [];
+    return this.trans(this.parse(s));
+  }
+
+  List<Token> parse(final String s) {
+    final List<Token> t = [];
+
     for (var m in this._expLine.allMatches(s)) {
-      // final label = m.group(1);
+      final label = m.group(1);
       final code = m.group(2);
       final operand = m.group(3);
 
       // FIXME
       switch (code) {
         case 'LD':
-          instructions.addAll(this.ld(operand));
+          t.add(this.ld(label, operand));
           break;
         case 'RET':
-          instructions.add(0x81 << 8);
+          t.add(this.ret(label));
           break;
         case 'NOP':
         default:
-          instructions.add(0);
+          t.add(this.nop(label));
       }
     }
-    return instructions;
+    return t;
   }
 
-  List<int> ld(final String operand) {
+  List<int> trans(final List<Token> tokens) {
+    final List<int> c = [];
+    for (var t in tokens) {
+      c.addAll(t.code);
+    }
+    return c;
+  }
+
+  Token ret(final String label) => Token([0x8100], label: label);
+
+  Token nop(final String label) => Token([0], label: label);
+
+  Token ld(final String label, final String operand) {
     final m = this._expCommonOperand.firstMatch(operand);
     final r1 = m.group(1);
     final r2 = m.group(2);
     final x = m.group(3);
 
     if (r1 == null || r2 == null) {
-      return [];
+      return this.nop(label);
     }
 
     // LD r1,r2
@@ -46,16 +81,16 @@ class Parse {
       var op = 0x14 << 8;
       var r = this._expGR.firstMatch(r1).group(1);
       if (r == null) {
-        return [];
+        return this.nop(label);
       }
       op |= int.parse(r) << 4;
 
       r = this._expGR.firstMatch(r2).group(1);
       if (r == null) {
-        return [];
+        return this.nop(label);
       }
       op |= int.parse(r);
-      return [op];
+      return Token([op], label: label);
     }
 
     // LD r,adr,x
@@ -63,25 +98,32 @@ class Parse {
       var op = 0x10 << 8;
       var r = this._expGR.firstMatch(r1).group(1);
       if (r == null) {
-        return [];
+        return this.nop(label);
       }
       op |= int.parse(r) << 4;
 
       if (x != null && this._expGR.hasMatch(x)) {
         r = this._expGR.firstMatch(x).group(1);
         if (r == null) {
-          return [];
+          return this.nop(label);
         }
         op |= int.parse(r);
       }
 
-      var adr = this._expADR.firstMatch(r2).group(1);
-      if (adr == null) {
-        return [];
+      if (this._expADR.hasMatch(r2)) {
+        var adr = this._expADR.firstMatch(r2).group(1);
+        if (adr == null) {
+          return this.nop(label);
+        }
+        adr = adr.replaceFirst('#', '0x');
+        return Token([op, int.parse(adr)], label: label);
       }
-      adr = adr.replaceFirst('#', '0x');
-      return [op, int.parse(adr)];
+      return Token(
+        [op, 0],
+        label: label,
+        refLabel: r2,
+      );
     }
-    return [];
+    return this.nop(label);
   }
 }
--- test/parse_test.dart
+++ test/parse_test.dart
@@ -14,17 +14,43 @@ void main() {
     final p = Parse();
     final code = p.exec(asm);
 
-    expect(code, equals([0, 0x81 << 8]));
+    expect(code, equals([0, 0x8100]));
+  });
+
+  test('NOP', () {
+    final p = Parse();
+    final actual = p.nop('LABEL');
+    final expected = Token([0], label: 'LABEL');
+
+    expect(actual.code, equals(expected.code));
+    expect(actual.label, equals(expected.label));
+    expect(actual.refLabel, equals(expected.refLabel));
+  });
+
+  test('RET', () {
+    final p = Parse();
+    final actual = p.ret('LABEL');
+    final expected = Token([0x8100], label: 'LABEL');
+
+    expect(actual.code, equals(expected.code));
+    expect(actual.label, equals(expected.label));
+    expect(actual.refLabel, equals(expected.refLabel));
   });
 
   test('LD', () {
     final p = Parse();
 
-    expect(p.ld('GR0,GR1'), equals([0x1401]));
-    expect(p.ld('GR2,GR0'), equals([0x1420]));
-    expect(p.ld('GR7,1234'), equals([0x1070, 1234]));
-    expect(p.ld('GR6,#1234'), equals([0x1060, 0x1234]));
-    expect(p.ld('GR4,#4321,GR5'), equals([0x1045, 0x4321]));
+    expect(p.ld('', 'GR0,GR1').code, equals([0x1401]));
+    expect(p.ld('', 'GR2,GR0').code, equals([0x1420]));
+    expect(p.ld('', 'GR7,1234').code, equals([0x1070, 1234]));
+    expect(p.ld('', 'GR6,#1234').code, equals([0x1060, 0x1234]));
+    expect(p.ld('', 'GR4,#4321,GR5').code, equals([0x1045, 0x4321]));
+
+    final actual = p.ld('LABEL', 'GR4,REF,GR5');
+    final expected = Token([0x1045, 0], label: 'LABEL', refLabel: 'REF');
+    expect(actual.code, equals(expected.code));
+    expect(actual.label, equals(expected.label));
+    expect(actual.refLabel, equals(expected.refLabel));
 
     const asm = '; ld test'
         '\tLD GR0,GR1\n'
