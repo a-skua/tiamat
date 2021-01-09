7c75d9b5a3c8a74410920659d0988fc3fd0101ba
asuka yagi <yagi.asuka.0x06@gmail.com>
2021-01-09 23:13:03 +0900

    fix web layout

--- web/src/app.dart
+++ web/src/app.dart
@@ -2,17 +2,48 @@ import 'dart:html';
 
 import 'package:tiamat/tiamat.dart';
 
-import './component/register.dart';
+import './component/resource_state.dart';
+import './component/control_panel.dart';
 
 Element app() {
   final cc = Casl2();
   final r = Resource();
   final c = Comet2();
 
-  final textarea = TextAreaElement()..nodes.add(Text(asm));
-  final output = TextAreaElement()..disabled = true;
-  final input = TextAreaElement();
   final inputValues = <String>[];
+
+  final state = ResourceState(r);
+  final editor = TextAreaElement()
+    ..style.width = '100%'
+    ..style.height = '100%'
+    ..style.resize = 'none'
+    ..nodes.add(Text(asm));
+  final output = TextAreaElement()
+    ..style.width = '100%'
+    ..style.height = '100%'
+    ..style.resize = 'none'
+    ..disabled = true;
+  final input = TextAreaElement()
+    ..style.width = '100%'
+    ..style.height = '100%'
+    ..style.resize = 'none';
+  final control = ControlPanel(
+    r,
+    c,
+    cc,
+    onPreExecute: () {
+      inputValues.clear();
+      inputValues.addAll((input.value ?? '').split('\n'));
+      r.PR = 0;
+      r.SP = -1; // TODO bug
+    },
+    onUpdate: () {
+      state.update();
+    },
+    getCode: () {
+      return editor.value ?? '';
+    },
+  );
 
   c.sv
     ..write = (s) {
@@ -23,68 +54,30 @@ Element app() {
       var x = 0;
       return () => s[x++ % s.length];
     }();
-  final registers = [
-    for (var i = 0; i < 8; i++)
-      Register(
-        'GR$i',
-        () => r.getGR(i),
-        (v) => r.setGR(i, v),
-      ),
-    Register(
-      'SP',
-      () => r.SP,
-      (v) => r.SP = v,
-    ),
-    Register(
-      'PR',
-      () => r.PR,
-      (v) => r.PR = v,
-    ),
-    Register(
-      'OF',
-      () => r.OF ? 1 : 0,
-      (v) => r.OF = v > 0,
-      bits: 1,
-      hasSigned: false,
-    ),
-    Register(
-      'SF',
-      () => r.SF ? 1 : 0,
-      (v) => r.SF = v > 0,
-      bits: 1,
-      hasSigned: false,
-    ),
-    Register(
-      'ZF',
-      () => r.ZF ? 1 : 0,
-      (v) => r.ZF = v > 0,
-      bits: 1,
-      hasSigned: false,
-    ),
-  ];
 
   return Element.div()
+    ..style.display = 'grid'
     ..nodes = [
-      textarea,
-      output,
-      input,
-      ButtonElement()
-        ..nodes.add(Text('execute'))
-        ..onClick.listen((_) {
-          inputValues.clear();
-          inputValues.addAll((input.value ?? '').split('\n'));
-          r.PR = 0;
-          r.SP = -1; // TODO bug
-          final code = cc.compile(textarea.value ?? '');
-          for (var i = 0; i < code.length; i++) {
-            r.memory.setWord(r.PR + i, code[i]);
-          }
-          c.exec(r);
-          for (final r in registers) {
-            r.update();
-          }
-        }),
-      for (final r in registers) r.render(),
+      Element.div()
+        ..style.gridColumn = '1 / 3'
+        ..style.gridRow = '1'
+        ..nodes = [control.render()],
+      Element.div()
+        ..style.gridColumn = '1'
+        ..style.gridRow = '2 / 4'
+        ..nodes = [editor],
+      Element.div()
+        ..style.gridColumn = '2'
+        ..style.gridRow = '2'
+        ..nodes = [output],
+      Element.div()
+        ..style.gridColumn = '2'
+        ..style.gridRow = '3'
+        ..nodes = [input],
+      Element.div()
+        ..style.gridColumn = '1 / 3'
+        ..style.gridRow = '4'
+        ..nodes = [state.render()],
     ];
 }
 
